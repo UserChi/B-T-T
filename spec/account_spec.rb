911d83147554663b9cd65f36143abcb0605ec4a3
UserChi <117730509+UserChi@users.noreply.github.com>
2023-02-26 18:09:08 +0000

    just making sure i committed

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -38,57 +38,6 @@ RSpec.describe Account do
         expect { account.print_statement }.to output("date || credit || debit || balance\n#{Time.now.strftime("%d/%m/%Y")} || 1000.00 ||  || 1000.00\n").to_stdout
       end
     end
- 
-  
-
-# RSpec.describe Account do
-#     describe "has a default balance of 0" do
-#       it "returns 0 as the current balance" do
-#         account = Account.new
-#         expect(account.current_bal).to eq(0)
-#       end
-#     end
-#   end
-
-# RSpec.describe Account do
-#     describe "#deposit" do
-#       it "adds 100 to the balance" do
-#         account = Account.new
-#         account.deposit(100)
-#         expect(account.current_bal).to eq(100)
-#       end
-#     end
-#   end
-  
-# RSpec.describe Account do
-#     describe "#withdraw" do
-#      it "adds 100 then subtracts 50 from the balance" do
-#       account = Account.new
-#       account.deposit(100)
-#       account.withdraw(50)
-#       expect(account.current_bal).to eq(50)
-#      end
-#     end
-  
-#     it "raises error when you try to withdraw more than the current balance" do
-#       account = Account.new
-#       account.deposit(100)
-#       expect { account.withdraw(200) }.to raise_error(RuntimeError, 'Your withdrawal exceeds your current balance.')
-#     end
-#   end 
-
-#   RSpec.describe Account do
-#    describe 'Printing the statement' do
-#     it 'grabs previous deposit and prints out the bank statement incl. date, transaction and current balance.' do 
-#     account.deposit(1000)
-#     statement = <<~STATEMENT
-#     date || credit || debit || balance
-#     #{@time_now} || 1000.00 || || 1000.00
-    
-#     STATEMENT
-    
-#     expect { account.statement }.to output(statement).to_stdout
-#     # end
 
     it 'grabs previous transactions and prints out the bank statement incl. date, transaction and current balance.' do
         account.deposit(1000)
