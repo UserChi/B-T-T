2a900b280f02b516829455ab360bc0cf43c03ea7
UserChi <117730509+UserChi@users.noreply.github.com>
2023-02-26 19:57:20 +0000

    fixed the statmenent prints correct format test just removed the extra space when printing a credit transaction

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,19 +1,20 @@
 class Statement
 
-    def self.print(transactions)
-      puts "date || credit || debit || balance\n"
-      transactions.each do |transaction|
-        if is_credit?(transaction)
-          puts "#{transaction.date} ||    ||  #{'%.2f' % transaction.amount}  ||  #{'%.2f' % transaction.current_bal}"
-        else
-          puts "#{transaction.date} ||  #{'%.2f' % transaction.amount}  ||    ||  #{'%.2f' % transaction.current_bal}"
-        end
+  def self.print(transactions)
+    puts "date || credit || debit || balance\n"
+    transactions.each do |transaction|
+      if is_credit?(transaction)
+        puts "#{transaction.date} ||    || #{'%.2f' % transaction.amount} || #{'%.2f' % transaction.current_bal}"
+      else
+        puts "#{transaction.date} || #{'%.2f' % transaction.amount} || || #{'%.2f' % transaction.current_bal}"
       end
     end
-  
-    def self.is_credit?(transaction)
-      true if transaction.type
-    end
   end
+
+  def self.is_credit?(transaction)
+    true if transaction.type
+  end
+end
+
   
 
