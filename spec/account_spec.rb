bd385b32494b8a1afb68b702f279bc478a1d9f80
UserChi <117730509+UserChi@users.noreply.github.com>
2023-02-26 18:07:58 +0000

    tests have turnt green for account spec so i refactord it so it doesn't have rspec.describe on every test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,67 +8,87 @@ RSpec.describe Account do
         # before aks the database before each spec
         @time_now = Time.now.strftime('%d/%m/%y')
         # time format according to the what i put in the given format string.
-    end
-  end
 
-RSpec.describe Account do
-    describe "has a default balance of 0" do
-      it "returns 0 as the current balance" do
-        account = Account.new
-        expect(account.current_bal).to eq(0)
-      end
-    end
-  end
-
-  RSpec.describe Account do
-    describe "#deposit" do
-      it "adds 100 to the balance" do
-        account = Account.new
+  
+    describe "deposit" do
+      it "adds the deposited amount to the balance" do
         account.deposit(100)
         expect(account.current_bal).to eq(100)
       end
     end
-  end
   
+    describe "withdraw" do
+      it "subtracts the withdrawn amount from the balance" do
+        account.deposit(200)
+        account.withdraw(100)
+        expect(account.current_bal).to eq(100)
+      end
   
-# describe'Balance' do
-#     it 'has a default balance of 0' do
-#         expect(account.current_bal).to eq(0)
-#     end
-# end
+      it "raises an error when you try to withdraw more than the current balance" do
+        expect { account.withdraw(200) }.to raise_error 'Your withdrawal exceeds your current balance.'
+      end
+    end
+  
+    describe "Printing the statement" do
+      before do
+        account.deposit(1000)
+      end
+  
+      it "grabs previous deposit and prints out the bank statement incl. date, transaction and current balance" do
+        expect { account.print_statement }.to output("date || credit || debit || balance\n#{Time.now.strftime("%d/%m/%Y")} || 1000.00 ||  || 1000.00\n").to_stdout
+      end
+    end
+ 
+  
 
-# describe 'Deposit' do
-#     it 'adds 100 to the balance' do
-#       account.deposit(100)
-#       expect(account.current_bal).to eq(100)
+# RSpec.describe Account do
+#     describe "has a default balance of 0" do
+#       it "returns 0 as the current balance" do
+#         account = Account.new
+#         expect(account.current_bal).to eq(0)
+#       end
 #     end
 #   end
 
-  describe 'Withdraw' do
-    it 'adds 100 then subtracts 50 from the balance' do
-      account.deposit(100)
-      account.withdraw(50)
-      expect(account.current_bal).to eq(50)
-    end
+# RSpec.describe Account do
+#     describe "#deposit" do
+#       it "adds 100 to the balance" do
+#         account = Account.new
+#         account.deposit(100)
+#         expect(account.current_bal).to eq(100)
+#       end
+#     end
+#   end
+  
+# RSpec.describe Account do
+#     describe "#withdraw" do
+#      it "adds 100 then subtracts 50 from the balance" do
+#       account = Account.new
+#       account.deposit(100)
+#       account.withdraw(50)
+#       expect(account.current_bal).to eq(50)
+#      end
+#     end
+  
+#     it "raises error when you try to withdraw more than the current balance" do
+#       account = Account.new
+#       account.deposit(100)
+#       expect { account.withdraw(200) }.to raise_error(RuntimeError, 'Your withdrawal exceeds your current balance.')
+#     end
+#   end 
 
-    it 'raises error when you try to withdraw more money than current balance.' do
-      account.deposit(100)
-      expect { account.withdraw(200) }.to raise_error 'Your withdrawal exceeds your current balance.'
-    end
-  end
-
-  describe 'Printing the statement' do
-
-    it 'grabs previous deposit and prints out the bank statement incl. date, transaction and current balance.' do 
-    account.deposit(1000)
-    statement = <<~STATEMENT
-    date || credit || debit || balance
-    #{@time_now} || 1000.00 || || 1000.00
+#   RSpec.describe Account do
+#    describe 'Printing the statement' do
+#     it 'grabs previous deposit and prints out the bank statement incl. date, transaction and current balance.' do 
+#     account.deposit(1000)
+#     statement = <<~STATEMENT
+#     date || credit || debit || balance
+#     #{@time_now} || 1000.00 || || 1000.00
     
-    STATEMENT
+#     STATEMENT
     
-    expect { account.statement }.to output(statement).to_stdout
-    # end
+#     expect { account.statement }.to output(statement).to_stdout
+#     # end
 
     it 'grabs previous transactions and prints out the bank statement incl. date, transaction and current balance.' do
         account.deposit(1000)
